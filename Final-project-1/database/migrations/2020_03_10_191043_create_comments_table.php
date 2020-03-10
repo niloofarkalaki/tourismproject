9abababc56c265197717f0cdee01ab20c8ad0ef1
niloofarkalaki <60093739+niloofarkalaki@users.noreply.github.com>
2020-03-10 23:35:24 +0330

    s

---
+++ Final-project-1/database/migrations/2020_03_10_191043_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+            $table->string('content');
+            $table->index('user_id');
+            $table->index('post_id');
+            $table->integer('status');
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
