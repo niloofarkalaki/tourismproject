16ff4218cc0e9177cfc5fb5f2deca9f5a742e311
atiyemogharnas <56082033+atiyemogharnas@users.noreply.github.com>
2020-09-11 11:51:21 +0430

    front search

--- Final-project-1/resources/views/search.blade.php
+++ Final-project-1/resources/views/search.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.app')
 @section('title',' خانه')
 @section('content')
-	<div class="card">
-    <div class="card-header"><b> نتایج جستجو برای "{{ request('query') }}":{{ $searchResults->count() }} </b></div>
+<div style="padding-right:450px;text-align:center;font-size:18px;color:#001a33;">
+	<div class="card col-6" style="text-align:center">
+    <div class="card-header" style="text-align:center;font-size:30px; font-weight: bold; text-shadow: 2px 2px 5px blue"><b> نتایج جستجو برای "{{ request('query') }}":{{ $searchResults->count() }} </b></div>
 
     <div class="card-body">
 
         @foreach($searchResults->groupByType() as $type => $modelSearchResults)
-            <h2>{{ ucfirst($type) }}</h2>
+            <h2 style="text-align:center;color:#001a33;">{{ ucfirst($type) }}</h2>
 
             @foreach($modelSearchResults as $searchResult)
                 <ul>
@@ -17,5 +18,6 @@
         @endforeach
 
     </div>
+</div>
 </div>
 @endsection
